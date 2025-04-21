c695efacd9fa661c050942b7234e7c304503d427
pinovha <kajfunretro12@gmail.com>
2025-04-21 17:56:44 +0200

    add created/modified timestamps and update function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import json
+import datetime
 
 def add_task(args, status="in-progress"):
     task_data = read_json()
@@ -9,9 +10,13 @@ def add_task(args, status="in-progress"):
     else:
         new_id = 1
 
+    current_time = datetime.datetime.now().strftime("%d/%m/%Y")
+
     task_data[new_id] = {
         "description": args.description,
-        "status": status 
+        "status": status, 
+        "date": current_time,
+        "last_modified": None
     }
     
     write_json(task_data)
@@ -22,14 +27,11 @@ def add_task(args, status="in-progress"):
         """.format(args.description, new_id))
 
 def del_task(args):
-    task_id = args.task_id
+    task_id = args.id
     
     task_data = read_json()
    
-    if not task_data:
-        print("""
-        Task list is empty.
-        """)
+    if is_empty(task_data):
         return
 
     task_content = task_data.pop(task_id, None)
@@ -44,20 +46,63 @@ def del_task(args):
         There is no task with given ID.
         """)
 
-def list_tasks(args):
+def update_task(args):
+    task_id = args.id
     task_data = read_json()
-    if task_data:
-        for task_id, task in task_data.items():
-            print((""" 
-        Id: {}
-        Description: "{}" 
-        Status: {} 
-        """.format(task_id, task["description"], task["status"])))
+
+    if is_empty(task_data):
+        return
+
+    if task_id in task_data:
+        description = args.description
+        task = task_data[task_id]
+
+        old_description = task["description"]
+        task["description"] = description
+
+        current_date = datetime.datetime.now().strftime("%d/%m/%Y")
+        task["last_modified"] = current_date
+        
+        write_json(task_data)
+
+        print("""
+        Task with ID: {} has been updated!
+        Old description: "{}"
+        New description: "{}"
+        """.format(task_id, old_description, task["description"]))
     else:
         print("""
-        There are no tasks.
-        Add first by using: task-cli add "task" 
+        There is no task with given ID.
         """)
+
+def is_empty(data):
+    if not data:
+        print("""
+        Task list is empty.
+        Add first by using: task-cli add "task"
+        """)
+        return True
+    return False
+
+
+def list_tasks(args):
+    task_data = read_json()
+        
+    if is_empty(task_data):
+        return
+  
+    for task_id, task in task_data.items():
+        last_modified = task["last_modified"]
+        if last_modified == None:
+            last_modified = "No changes yet."
+        
+        print((""" 
+        Id: {}
+        Description: "{}" 
+        Status: {}
+        Created: {}
+        Last Modified: {}
+        """.format(task_id, task["description"], task["status"], task["date"], last_modified)))
         
 def read_json():
     if os.path.exists("data.json") and os.path.getsize("data.json") > 0:
@@ -81,8 +126,14 @@ def main():
 
     # Delete task
     del_parser = subparsers.add_parser("del", help="Deletes a specific task.")
-    del_parser.add_argument("task_id", help="Id of the task you want to delete.")
+    del_parser.add_argument("id", help="Id of the task you want to delete.")
     del_parser.set_defaults(func=del_task)
+
+    # Update task
+    update_parser = subparsers.add_parser("update", help="Updates a specific task.")
+    update_parser.add_argument("id", help="Id of the task you want to upgrade.")
+    update_parser.add_argument("description", help="New description of the task.")
+    update_parser.set_defaults(func=update_task) 
 
     # Wyświetlanie listy zadań
     list_parser = subparsers.add_parser("list", help="Wyświetla listę wszystkich zadań.")
