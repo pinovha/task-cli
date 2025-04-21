b61dc324ebd25de82f16de605c01dd760735205d
pinovha <kajfunretro12@gmail.com>
2025-04-21 15:20:05 +0200

    refactor JSON structure and add delete task function

--- main.py
+++ main.py
@@ -5,40 +5,66 @@ import json
 def add_task(args, status="in-progress"):
     task_data = read_json()
     if task_data:
-        new_id = max(task["id"] for task in task_data) + 1
+        new_id = max(int(task_id) for task_id in task_data.keys()) + 1
     else:
         new_id = 1
 
-    new_task = {
-        "id": new_id,
-        "description": args.task_content,
+    task_data[new_id] = {
+        "description": args.description,
         "status": status 
     }
     
-    task_data.append(new_task)
     write_json(task_data)
 
     print(""" 
-    Zadanie {} zostało zapisane.
+    Zadanie "{}" zostało zapisane.
     Id: {}.
-    """.format(args.task_content, new_id))
+    """.format(args.description, new_id))
+
+def del_task(args):
+    task_id = args.task_id
+    
+    task_data = read_json()
+   
+    if not task_data:
+        print("""
+        Task list is empty.
+        """)
+        return
+
+    if task_id in task_data:
+        task_content = task_data[task_id]
+        task_data.pop(task_id)
+        write_json(task_data)
+        print("""
+        Task "{}", with ID: {} has been deleted.
+        """.format(task_content["description"], task_id))
+    else:
+        print("""
+        There is no task with given ID.
+        """)
 
 def list_tasks(args):
     task_data = read_json()
-    for item in task_data:
-        id, description, status = item.values()
-        print(""" 
+    if task_data:
+        for task_id, task in task_data.items():
+            print((""" 
         Id: {}
-        Opis: {}
-        Status: {}
-        """.format(id, description, status))
+        Description: "{}" 
+        Status: {} 
+        """.format(task_id, task["description"], task["status"])))
+    else:
+        print("""
+        There are no tasks.
+        Add first by using: task-cli add "task" 
+        """)
         
 def read_json():
     if os.path.exists("data.json") and os.path.getsize("data.json") > 0:
         with open('data.json', 'r') as f:
             return json.load(f) 
     else:
-        return []
+        return {}
 
 def write_json(data):
     with open("data.json", "w") as f:
@@ -50,8 +76,13 @@ def main():
 
     # Dodawanie zadania
     add_parser = subparsers.add_parser("add", help="Dodaje nowe zadanie.")
-    add_parser.add_argument("task_content", help="Treść zadania")
+    add_parser.add_argument("description", help="Treść zadania")
     add_parser.set_defaults(func=add_task)
+
+    # Delete task
+    add_parser = subparsers.add_parser("del", help="Deletes a specific task.")
+    add_parser.add_argument("task_id", help="Id of the task you want to delete.")
+    add_parser.set_defaults(func=del_task)
 
     # Wyświetlanie listy zadań
     add_parser = subparsers.add_parser("list", help="Wyświetla listę wszystkich zadań.")
