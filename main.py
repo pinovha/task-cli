9a8fcb2071e654c81717059bfdc757e73e683165
Kamil <kajfunretro12@gmail.com>
2025-05-13 14:22:28 +0200

    change JSON structure to be more readable and efficient

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from argparse import ArgumentParser
 import os
 import json
 import datetime
-from typing import Dict
+from typing import Dict, Any, List
 
 DATA_FILE = "data.json"
 STATUS_CHOICES = ["todo", "in-progress", "done"]
@@ -13,12 +13,12 @@ class TaskManager:
         self.data_file = data_file
         self.tasks = self._read_tasks()
 
-    def _read_tasks(self) -> Dict[str, Dict[str, str]]:
+    def _read_tasks(self) -> List[Dict[str, Any]]:
         if os.path.exists(self.data_file) and os.path.getsize(self.data_file) > 0:
             with open(self.data_file, 'r') as f:
                 return json.load(f) 
         else:
-            return {}
+            return []
 
     def _save_tasks(self) -> None:
         with open(self.data_file, "w") as f:
@@ -35,54 +35,57 @@ class TaskManager:
 
     def add_task(self, description: str) -> None:
         if self.tasks:
-            new_id: str = str(max(int(task_id) for task_id in self.tasks.keys()) + 1)
+            new_id: int = max(task["id"] for task in self.tasks) + 1
         else:
-            new_id: str = "1"
+            new_id: int = 1
 
-        self.tasks[new_id] = {
+        self.tasks.append({
+            "id": new_id,
             "description": description,
             "status": "todo", 
             "createdAt": datetime.datetime.now().strftime(DATE_FORMAT),
-            "updatedAt": ""
-        }
-        self._save_tasks()   
-        print(""" 
-            Task "{}" has been saved. With id = {}.
-            """.format(description, new_id))
+            "updatedAt": None 
+        })
+        self._save_tasks()
+        print(f""" 
+            Task "{description}" has been saved. With id = {new_id}.
+            """)
 
-    def del_task(self, task_id: str) -> None:
+    def del_task(self, task_id: int) -> None:
         if self._is_task_list_empty():
             return
 
-        task_content = self.tasks.pop(task_id, None)
+        for i, task in enumerate(self.tasks):
+            if task["id"] == task_id:
+                task_content = self.tasks.pop(i)
+                self._save_tasks()
+                print(f""" 
+            Task "{task_content["description"]}" has been deleted.
+            """)
+                return
 
-        if task_content:
-            self._save_tasks()
-            print("""
-            Task "{}", with ID: {} has been deleted.
-            """.format(task_content["description"], task_id))
-        else:
-            print("""
+        print("""
             There is no task with given ID.
             """)
 
-    def update_task(self, task_id: str, description: str) -> None:
+    def update_task(self, task_id: int, description: str) -> None:
         if self._is_task_list_empty():
             return
 
-        if task_id in self.tasks:
-            task = self.tasks[task_id]
-            old_description = task["description"]
-            task["description"] = description
-            task["updatedAt"] = datetime.datetime.now().strftime(DATE_FORMAT)
-            self._save_tasks()
-            print("""
-            Task with ID: {} has been updated!
-            Old description: "{}"
-            New description: "{}"
-            """.format(task_id, old_description, description))
-        else:
-            print("""
+        for task in self.tasks:
+            if task["id"] == task_id:
+                old_description = task["description"]
+                task["description"] = description
+                task["updatedAt"] = datetime.datetime.now().strftime(DATE_FORMAT)
+                self._save_tasks()
+                print(f"""
+            Task with ID: {task_id} has been updated!
+            Old description: "{old_description}"
+            New description: "{description}"
+            """)
+                return
+
+        print("""
             There is no task with given ID.
             """)
 
@@ -92,43 +95,42 @@ class TaskManager:
         
         filtered_tasks = self.tasks
         if status:
-            filtered_tasks = {task_id: task for task_id, task in self.tasks.items() if task["status"] == status}
+            filtered_tasks = [task for task in self.tasks if task["status"] == status]
     
         if not filtered_tasks:
-            print("""
-            No tasks with status: {}
-            """.format(status))
+            print(f"""
+            No tasks with status: {status}
+            """)
+            return
 
-        for task_id, task in filtered_tasks.items():
-            updatedAt = task["updatedAt"]
-            if updatedAt == "":
-                updatedAt = "No changes recorded."
+        for task in self.tasks: 
+            updatedAt = task["updatedAt"] if task["updatedAt"] else "No changes recorded."
             
-            print((""" 
-            Id: {}
-            Description: "{}" 
-            Status: {}
-            Created: {}
-            Last Modified: {}
-            """.format(task_id, task["description"], task["status"], task["createdAt"], updatedAt)))
+            print(f""" 
+            Id: {task["id"]}
+            Description: "{task["description"]}" 
+            Status: {task["status"]}
+            Created: {task["createdAt"]}
+            Last Modified: {updatedAt}
+            """)
             
-    def mark_task(self, task_id: str, status: str) -> None:
+    def mark_task(self, task_id: int, status: str) -> None:
         if self._is_task_list_empty():
             return
 
-        if task_id not in self.tasks:
-            print("""
-            Task with ID: {} does not exist.
-            """.format(task_id))
-            return
+        for task in self.tasks:
+            if task["id"] == task_id:
+                task["status"] = status
+                task["updatedAt"] = datetime.datetime.now().strftime(DATE_FORMAT)
+                self._save_tasks()
+                print(f"""
+            Task "{task["description"]}" marked as {status}.
+            """)
+                return
 
-        task = self.tasks[task_id]
-        task["status"] = status
-        task["updatedAt"] = datetime.datetime.now().strftime(DATA_FILE)
-        self._save_tasks()
-        print("""
-            Task "{}" marked as {}.
-            """.format(task["description"], status))
+        print(f"""
+            Task with ID: {task_id} does not exist.
+            """)
 
 def create_parser() -> ArgumentParser:
     parser = ArgumentParser(prog="task-cli", description="This program allows you to manage tasks.") 
@@ -141,19 +143,19 @@ def create_parser() -> ArgumentParser:
 
     # Delete task
     del_parser = subparsers.add_parser("del", help="Delete a specific task.")
-    del_parser.add_argument("id", help="Id of the task you want to delete.")
+    del_parser.add_argument("id", type=int, help="Id of the task you want to delete.")
     del_parser.set_defaults(action="del")
 
     # Update task
     update_parser = subparsers.add_parser("update", help="Update a specific task.")
-    update_parser.add_argument("id", help="Id of the task you want to upgrade.")
+    update_parser.add_argument("id", type=int, help="Id of the task you want to upgrade.")
     update_parser.add_argument("description", help="New description of the task.")
     update_parser.set_defaults(action="update") 
 
     # Mark task
     mark_parser = subparsers.add_parser("mark", help="Marks the task.")
-    mark_parser.add_argument("status", choices=STATUS_CHOICES, help="Choose status: in-progress, done.")
-    mark_parser.add_argument("id", help="Id of the task you want to mark.")
+    mark_parser.add_argument("id", type=int, help="Id of the task you want to mark.")
+    mark_parser.add_argument("status", choices=STATUS_CHOICES)
     mark_parser.set_defaults(action="mark")
 
     # List tasks
